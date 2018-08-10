ad93df6c9233ce0a4a48992edd90ceb47d62b129
Harry Phillips <harryjamesphillips@gmail.com>
2018-08-10 10:23:56 +0100

    debug1

--- burstLibrary/src/test/java/com/harrysoft/burstcoinexplorer/util/SingleTestUtils.java
+++ burstLibrary/src/test/java/com/harrysoft/burstcoinexplorer/util/SingleTestUtils.java
@@ -3,14 +3,14 @@ package com.harrysoft.burstcoinexplorer.util;
 import io.reactivex.Single;
 import io.reactivex.observers.TestObserver;
 
-import static junit.framework.Assert.assertEquals;
-import static junit.framework.Assert.assertNotNull;
+import static junit.framework.Assert.*;
 
 public class SingleTestUtils {
     public static <T> T testSingle(Single<T> single) {
         assertNotNull(single);
         TestObserver<T> observer = single.test();
-        assertEquals(true, observer.awaitTerminalEvent());
+        assertTrue(observer.awaitTerminalEvent());
+        observer.errors().forEach(Throwable::printStackTrace);
         observer.assertNoErrors();
         T object = observer.values().get(0);
         assertNotNull(object);
